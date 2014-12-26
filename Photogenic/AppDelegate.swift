0618049b13c432e9f3e21d6ba56ead07632657ff
Joseph Daniels <josephdaniels@Josephs-MBP.home>
2014-12-26 00:01:57 -0500

    initial commit

---
+++ Photogenic/AppDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  AppDelegate.swift
+//  Photogenic
+//
+//  Created by Joseph Daniels on 12/25/14.
+//  Copyright (c) 2014 JosephDaniels. All rights reserved.
+//
+
+
+import Cocoa
+
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+    }
+
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+    }
+
+
+
+
+}
---
+++ Photogenic/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Photogenic
+//
+//  Created by Joseph Daniels on 12/25/14.
+//  Copyright (c) 2014 JosephDaniels. All rights reserved.
+//
+
+
+import Cocoa
+
+
+class ViewController: NSViewController {
+
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+    }
+
+
+    override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+    }
+
+
+
+
+}
